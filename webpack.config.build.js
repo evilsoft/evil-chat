3e7e9e6fe329ee234e4650446d0a57d2bd6fb08e
evilsoft <evilsoft@aol.com>
2015-08-31 20:22:59 -0700

    Initial Styles :sunglasses:

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -3,7 +3,7 @@ module.exports = {
   output: {
     path:           './dist/js',
     filename:       'app.js',
-    library:        'EvilApp',
+    library:        'EvilChat',
     libraryTarget:  'umd'
   },
   module: {
