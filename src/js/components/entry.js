2cdde90987347948c21edefee51c2f7463654ac8
evilsoft <evilsoft@aol.com>
2015-09-19 17:37:21 -0700

    set up input binding

--- src/js/components/entry.js
+++ src/js/components/entry.js
@@ -1,19 +1,27 @@
 import m from 'mithril';
 
-var Entry = {
-  view() {
-    return (
-      <div className="entry">
-        <input
-          className="entry__input"
-          autocomplete="off"
-        />
-        <button type="button" className="entry__send">
-          Send
-        </button>
-      </div>
-    );
-  }
-};
+function controller() {
+  return {
+    message: m.prop(''),
+  };
+}
+
+function view(ctrl) {
+  return (
+    <div className="entry">
+      <input
+        className="entry__input"
+        autocomplete="off"
+        onchange={m.withAttr('value', ctrl.message)}
+        value={ctrl.message()}
+      />
+      <button type="button" className="entry__send">
+        Send
+      </button>
+    </div>
+  );
+}
+
+const Entry = { controller, view };
 
 export default Entry;
